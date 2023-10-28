99613b8d9fbee5fba151564d88d5f94435e7b2b3
Sebastiaan Stok <s.stok@rollerscapes.net>
2023-10-28 20:55:14 +0200

    Init config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// See https://hupkit.github.io/hupkit/config.html
+
+return [
+    'branches' => [],
+    'host' => 'github.com',
+    'repository' => 'rollerworks/password-common-list',
+    'schema_version' => 2,
+];
